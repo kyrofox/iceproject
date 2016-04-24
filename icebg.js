6a8cce5c819856f16fa70546de717b3cf56848be
= <mark.muego@gmail.com>
2016-04-24 03:52:56 -0700

    50% functionality, able to tag things now and retrieve tags, cleaned up code, lots of stuff.

--- icebg.js
+++ icebg.js
@@ -1,13 +1,17 @@
 
 
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+	console.log("Got message from: " + sender.tab.id);
 	console.log(request);
 	if (request["from"] === "page") {
 		handlePageReq(request, sender, sendResponse);
 	} else if (request["from"] === "popup") {
 		//notyetbubs
 	}
+	return true;
 });
+
+
 		/*
 	console.log(sender.tab ?
 				"from a content script:" + sender.tab.url :
@@ -17,43 +21,95 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 */
 
 function handlePageReq(req, sender, sendResponse) {
-		if (req.type === "addTag") {
-			getGalleryPost(req.info.galleryId, function(resp) {
-				resp.tags.append(req.info.tag);
-				chrome.storage.local.set({[req.info.galleryId]: {
-					favorited: resp.favorited,
-					tags: resp.tags
-					time: new Date()
-				}});
-			});
-		} else if (req.type === "removeTag") {
+	function respond(resp) {
+		console.log("Sending response:");
+		console.log(resp);
+		//debugger;
+		sendResponse(resp);
+	}
+	
+	if (req.type === "addTag") { // yeah yeah i need to rename this... lazy :/
+		addTagToPost(req);
+	} else if (req.type === "removeTag") {
+		removeTagFromPost(req);
+	} else if (req.type === "getGalleryPost") {
+		getGalleryPost(req.info.galleryId, respond);
+	} else if (req.type === "getAllTags") {
+		getAllTags(respond);
+	} else if (req.type === "addNewTag") {
+		getAllTags(function(resp) {
+			if (resp.indexOf(req.info.tag) > -1 ) {
+				//tag already exists.
+				respond("fuck you");
+			} else {
+				resp.push(req.info.tag);
+				chrome.storage.local.set({"all_of_teh_tags": resp});
+				addTagToPost(req);
+				respond({success: true});
+			}
 			
-			getGalleryPost(req.info.galleryId, function(resp) {
-				resp.tags.remove(req.info.tag);
-				chrome.storage.local.set({[req.info.galleryId]: {
-					favorited: resp.favorited,
-					tags: resp.tags,
-					time: new Date()
-				}});
-			});
-			
-		} else if (req.type === "getGalleryPost") {
-			getGalleryPost(request.info.galleryId, function(resp) {
-				sendResponse(resp);
-			};
-		} else if (req.type === "getAllTags") {
-			sendResponse(["one", "two", "three"]);
-		}
+		});
+	}
 }
 
-function getAllTags() {
-	
+function addTagToPost(req) {
+	getGalleryPost(req.info.galleryId, function(resp) {
+		resp.tags.push(req.info.tag);
+		chrome.storage.local.set({[req.info.galleryId]: {
+			favorited: resp.favorited,
+			tags: resp.tags,
+			time: new Date().getTime()
+		}});
+	});
 }
+
+function removeTagFromPost(req) {
+	getGalleryPost(req.info.galleryId, function(resp) {
+		resp.tags.remove(req.info.tag);
+		chrome.storage.local.set(
+		{[req.info.galleryId]: {
+			favorited: resp.favorited,
+			tags: resp.tags,
+			time: new Date().getTime()
+		}});
+	});
+}
+
+function getAllTags(callback) {
+	chrome.storage.local.get("all_of_teh_tags", function(resp) {
+		if (typeof resp["all_of_teh_tags"] === "undefined") {
+			//debugger;
+			//shit don't exist, create it
+			chrome.storage.local.set({"all_of_teh_tags": []});
+			getAllTags(callback);
+		} else {
+			callback(resp["all_of_teh_tags"]);
+		}
+	});
+}
+/*HAw0r1n: {
+	favorited: true,
+	tags: ["okay", "pls", "nty"],
+	time: 123123123123213
+}*/
 
 function getGalleryPost(galleryId, callback) {
 	chrome.storage.local.get(galleryId, function(resp) {
-		if (response.galleryId === undefined)
-		callback(response[galleryId]);
+		console.log("ok")
+		console.log(resp);
+		if (typeof resp[galleryId] === "undefined") {
+			//debugger;
+			//shit don't exist, create it
+			chrome.storage.local.set({[galleryId]: {
+				favorited: null,
+				tags: [],
+				time: new Date().getTime()
+			}});
+			getGalleryPost(galleryId, callback);
+			
+		} else {
+			callback(resp[galleryId]);
+		}
 	});
 }
 
--- icepage.js
+++ icepage.js
@@ -12,7 +12,102 @@
     th.appendChild(s);
 })();
 
+function main() {
+	var iceTagMenu = $('<div id="iceTagMenu">' +
+		'<div id="iceTagList">' +
+		'</div>' +
+		'<input id="newTag" type="text" placeholder= "new tag? :P">' +
+		'</div>');
+	function makeTag(checked, tagName) {
+		var newTag = $('<span class="iceTag ' + (checked ? "checked" : "") + '"><span class="iceTagSquare">✔</span> <span class="iceTagText">' + tagName + '</span></span>' );
+		newTag.click(function() {
+			if (newTag.hasClass("checked")) {
+				//send message to background to remove tag
+				newTag.removeClass("checked");
+				msgBg("removeTag", { "galleryId": getGalleryId(), "tag": tagName	});
+			} else {
+				newTag.addClass("checked");
+				msgBg("addTag", { "galleryId": getGalleryId(), "tag": tagName });
+				//send message to background to add tag
+			}
+		});
+		return newTag;
+	}
+
+	msgBg("getGalleryPost", { galleryId: getGalleryId() }, function(galleryPost) {
+		console.log("galleryPost");
+		console.log(galleryPost);
+		msgBg("getAllTags", "", function(allTags) {
+			console.log(allTags);
+			for (var i = 0; i < allTags.length; i++) {
+				iceTagMenu.find("#iceTagList").append( makeTag( 
+					galleryPost.tags.indexOf(allTags[i]) > -1,
+					allTags[i]));
+			}
+		});
+	});
+	
+	$('#under-image').append(iceTagMenu);
+	$("#newTag").keypress(function (e) {
+		if (e.which == 13) {
+			// check if it exists already
+			// if it doesn't, add it to allTags
+			msgBg("addNewTag", {"tag": $("#newTag").val()}, function(resp) {
+				if (resp.success === true) {
+					msgBg("addTag", {
+						galleryId: getGalleryId(),
+						tag: $("#newTag").val()
+					});
+					$("#iceTagList").append(makeTag(true, $("#newTag").val()));
+				} else {
+					//bad stuff
+				}
+			});
+			// update view
+		}
+	});
+}
+
+function msgBg(type, jsonInfo, callback) {
+	var msg = {
+		"from": "page",
+		"type": type,
+		"info": jsonInfo
+	};
+	console.log("sending message: ");
+	console.log(msg);
+	chrome.runtime.sendMessage(msg, callback);
+}
+
+function getGalleryId() {//figure out a better way to do this, then set multiple ways to do it so if one fails use next.
+	var url = window.location.href;
+	return url.substring(url.indexOf("gallery/") + 8);
+}
+
+
+// perhaps every week or so delete old favorites?
+//<on every page>
+
 /*
+$("#under-image > div > span.favorite-image").click(function() {
+	if ($(this).hasClass("favorited")) {
+		//display menu element, position it adjacent to the top right corner of favorites button
+		//$('#under-image').append(iceTagMenu);
+	
+	}
+});
+function handleResponse(response) {
+	if (response.type === "gallery") {
+		
+	}
+}
+
+HAw0r1n: {
+	favorited: true,
+	tags: ["okay", "pls", "nty"],
+	time: 123123123123213
+}
+
 function waitUntilImgurLoads(callback) {
 	window.setTimeout(function() {
 		try {
@@ -23,8 +118,8 @@ function waitUntilImgurLoads(callback) {
 		
 		}
 	}, 100);
-}*/
-/*
+}
+
 (function () {
 	currentUser = {};
 	var msg = { 
@@ -43,116 +138,8 @@ function waitUntilImgurLoads(callback) {
 	}
 
 });
-*/
-function getAllTags(callback) {
-	msgBg("getAllTags", "", callback);
-}
-
-function handleResponse(response) {
-	if (response.type === "gallery") {
-		
-	}
-}
 
 
-function msgBg(type, jsonInfo, callback) {
-	var msg = {
-		"from": "page",
-		"type": type,			
-		"info": jsonInfo
-	};
-	console.log("sending message: ");
-	console.log(msg);
-	chrome.runtime.sendMessage(msg, callback);
-}
-
-
-
-
-// perhaps every week or so delete old favorites?
-
-//<on every page>
-var iceTagMenu = $('<div id="iceTagMenu">' +
-		'<div id="iceTagList">' +
-		'</div>' +
-		'<input type="text" placeholder= "new tag? :P">' +
-'</div>');
-
-$("#under-image > div > span.favorite-image").click(function() {
-	if ($(this).hasClass("favorited")) {
-		//display menu element, position it adjacent to the top right corner of favorites button
-		//$('#under-image').append(iceTagMenu);
-	
-	}
-});
-
-function makeTag(checked, tagName) {
-	var newTag = $('<span class="iceTag ' + (checked ? "checked" : "") + '"><span class="iceTagSquare">✔</span> <span class="iceTagText">' + tagName + '</span></span>' );
-	newTag.click(function() {
-		if (newTag.hasClass("checked")) {
-			newTag.removeClass("checked");
-			//send message to background to remove tag
-			msgBg("removeTag", {
-				galleryId: getGalleryId(),
-				tag: tagName
-			});
-		} else {
-			newTag.addClass("checked");
-			msgBg("addTag", {
-				galleryId: getGalleryId(),
-				tag: tagName
-			});
-			//send message to background to add tag
-		}
-	});
-	return newTag;
-}
-
-msgBg("getGalleryPost", { galleryId: getGalleryId() }, function(galleryPost) {
-	getAllTags(function(allTags) {
-		for (var i = 0; i < allTags.length; i++) {
-			iceTagMenu.find("#iceTagList").append( makeTag( 
-				galleryPost.tags.indexOf(allTags[i]) > -1,
-				allTags[i]
-			));
-		}
-	});
-	
-
-
-});
-
-
-
-
-/*
-iceTagMenu.find("#iceTagList").append(makeTag(false, "yo"));
-iceTagMenu.find("#iceTagList").append(makeTag(false, "HEYtherewhatsupdude"));
-iceTagMenu.find("#iceTagList").append(makeTag(false, "goodfriendshipsmustberoutinely"));
-iceTagMenu.find("#iceTagList").append(makeTag(false, "yo"));
-*/
-
-$('#under-image').append(iceTagMenu);
-/*HAw0r1n: {
-	favorited: true,
-	tags: ["okay", "pls", "nty"],
-	time: 123123123123213
-}*/
-console.log("hello");
-
-
-
-
-
-
-
-
-
-function getGalleryId() {//figure out a better way to do this, then set multiple ways to do it so if one fails use next.
-	var url = window.location.href;
-	
-	return url.substring(url.indexOf("gallery/") + 8);
-}
 // <display menu element>
 //message background script and ask it for what tags exist
 //
@@ -160,11 +147,11 @@ function getGalleryId() {//figure out a better way to do this, then set multiple
 //message background script and ask it for what tags the id has
 //
 
-/*
 
 httpGetAsync("http://imgur.com/account/favorites/newest/page/1/miss?scrolled", function(response) {
 	console.log(response);
 });
 
 var favBtn = document.querySelector();
-document.createElement("div");*/
+document.createElement("div");
+*/
